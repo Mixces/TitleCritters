584cc2ff8a9902dec45c7c4a272118e421287112
Mixces <aar.hcms@gmail.com>
2025-03-19 13:11:00 -0500

    initial commit 2

---
+++ src/main/java/me/mixces/titlecritters/TitleCritters.java
@@ -0,0 +1,11 @@
+package me.mixces.titlecritters;
+
+import net.ornithemc.osl.entrypoints.api.ModInitializer;
+
+public class TitleCritters implements ModInitializer {
+
+	@Override
+	public void init() {
+		System.out.println("TitleCritters initialized!");
+	}
+}
